74a8b6ef0cb981ba41ad8d54f42c48194825e7e5
Siya-Mwol <109611007+Siya-Mwol@users.noreply.github.com>
2023-11-25 13:08:05 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,22 +1,17 @@
-const {
-    command
-} = require("../lib/");
-const {
-    tiny
-} = require("@viper-x/fancytext")
+const { command} = require("../../lib/");
 command(
-    {
-        pattern: "ping",
-        fromMe: false,
-        desc: "Speed check ping",
-        type: "user",
-    },
-    async (message, match) => {
-        const start = new Date().getTime();
-        await message.sendMessage(tiny("*ping*"));
-        const end = new Date().getTime();
-        return await message.sendMessage(tiny(
-            "*Pong!*\n " + (end - start) + " *ms*"
-        ));
-    }
+  {
+    pattern: "ping",
+    fromMe: false,
+    desc: "To check ping",
+    type: "user",
+  },
+  async (message, match) => {
+    const start = new Date().getTime();
+    await message.sendMessage(message.jid,"```Ping!```");
+    const end = new Date().getTime();
+    return await message.sendMessage(message.jid,
+      "*Pong!*\n ```" + (end - start) + "``` *ms*"
+    );
+  }
 );
